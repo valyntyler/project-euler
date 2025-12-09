a08905070578db7615761c372ad7a53439895d2a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-09 01:21:51 +0100

    feat: 0000 (better)

--- 0000/main.py
+++ 0000/main.py
@@ -1,23 +1,13 @@
-from math import sqrt
-
-
 def odd(n: int) -> bool:
     return n % 2 != 0
 
 
-def perfect_square(n: int) -> bool:
-    return int(sqrt(n)) ** 2 == n
-
-
 sum = 0
-n = 1
-i = 446e3
+top = 446e3
 
-while i != 0:
-    if perfect_square(n):
-        sum += n * int(odd(n))
-        i -= 1
-        print(n)
-    n += 1
+for i in range(1, int(top) + 1):
+    print(i**2)
+    if odd(i):
+        sum += i**2
 
 print(sum)
